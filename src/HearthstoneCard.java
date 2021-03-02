85043f23fe3a3734ac3566cd3e03903b6c47008e
AdamHedman <78870297+AdamHedman@users.noreply.github.com>
2021-03-02 10:57:10 -0600

    update

--- src/HearthstoneCard.java
+++ src/HearthstoneCard.java
@@ -14,11 +14,26 @@ public class HearthstoneCard
 		this.name = name;
 	}
 	
+	public HearthstoneCard(String cardString)
+	{
+		String[] parts = cardString.split(",");
+		this.name = parts[0];
+		this.cost = Integer.parseInt(parts[1]);
+		this.attack = Integer.parseInt(parts[2]);
+		this.defense = Integer.parseInt(parts[3]);
+	}
+	
+	public String toCardString()
+	{
+		return "" + this.name + "," + this.cost + "," + this.attack + "," + this.defense;
+	}
+	
 	public int getCost()
 	{
 		return this.cost;
 	}
 	
+	//setters allow us to conditionally change the value of a private member
 	public void setName(String name)
 	{
 		if(name.length() >= 5)
@@ -29,6 +44,7 @@ public class HearthstoneCard
 	
 	void display()
 	{
+		//System.out.println("Name: " + this.name + "\nCost" + this.cost + "\nAttack: " + this.attack + " Defense: " + this.defense);
 		System.out.format("Name: %s \nCost: %d \nAttack: %d Defense: %d\n", this.name, this.cost, this.attack,this.defense);
 	}
 }
